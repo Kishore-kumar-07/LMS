cfe4fc1be12fe9df5612835004ed333d07f829c4
Kishore-kumar-07 <bkdon54@gmail.com>
2024-08-21 13:40:14 +0530

    table Component added

---
+++ frontend/src/pages/Employee/EmployeeHomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Table from './components/table'
+
+function EmployeeHomePage() {
+  return (
+    <>
+    <div>
+    <Table/> 
+    </div>
+ 
+    </>
+     
+   
+  )
+}
+
+export default EmployeeHomePage
