0d5ae8671b288d36cec09c7f15fb612f92b4e89c
unknown <vijayguhan10@gmail.com>
2024-08-21 23:25:14 +0530

    updated css

--- src/components/LeaveApplication.js
+++ src/components/LeaveApplication.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "../css/LeavAplication.css";
 
 const LeaveApplication = () => {
   const [selectedHalf, setSelectedHalf] = useState(null);
@@ -16,9 +15,9 @@ const LeaveApplication = () => {
 
   return (
     <div>
-      <div className="my-10 marginbottom flex flex-row">
+      <div className="my-6  flex flex-row">
         <div>
-          <h3 className="font-bold fontsizing">Select Leave to Apply</h3>
+          <h3 className="font-bold ">Select Leave to Apply</h3>
         </div>
         <div className="mx-20">
           <select
@@ -33,7 +32,7 @@ const LeaveApplication = () => {
         </div>
       </div>
       <div className="flex flex-row gap-x-32">
-        <div className="borderforform items-center roun">
+        <div className=" border border-spacing-2 border-black items-center roun">
           <label
             htmlFor="fromDate"
             className="block text-gray-700 font-bold mb-2 mx-12"
@@ -42,11 +41,11 @@ const LeaveApplication = () => {
           </label>
           <input
             id="fromDate"
-            className="backgroundcolor my-2 mx-12 p-2 border border-cyan-400 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-cyan-600"
+            className=" w-96 my-2 mx-12 p-2 border border-cyan-400 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-cyan-600"
             type="date"
             placeholder="From Date"
           ></input>
-          <div className="borderbottom"></div>
+          <div className=" border-b-2  border-black"></div>
           <div className="flex flex-row items-center justify-between mx-9 my-3">
             <div className="flex flex-row items-center">
               <h3 className="mr-4">First Half</h3>
@@ -76,7 +75,7 @@ const LeaveApplication = () => {
             </div>
           </div>
         </div>
-        <div className="borderforform items-center">
+        <div className="border border-spacing-2 border-black items-center">
           <label
             htmlFor="endDate"
             className="block text-gray-700 font-bold mb-2 mx-12"
@@ -85,11 +84,11 @@ const LeaveApplication = () => {
           </label>
           <input
             id="endDate"
-            className="backgroundcolor my-2 mx-12 p-2 border border-cyan-400 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-cyan-600"
+            className="w-96  my-2 mx-12 p-2 border border-cyan-400 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-cyan-600"
             type="date"
             placeholder="End Date"
           ></input>
-          <div className="borderbottom"></div>
+          <div className="border-b-2  border-black"></div>
           <div className="flex flex-row items-center justify-between mx-9 my-3">
             <div className="flex flex-row items-center">
               <h3 className="mr-4">First Half</h3>
@@ -120,7 +119,7 @@ const LeaveApplication = () => {
           </div>
         </div>
       </div>
-      <h3 className=" my-8 fontsizeing">Select Leave Reason</h3>
+      <h3 className=" my-8 ">Select Leave Reason</h3>
       <div className=" flex flex-row gap-x-32">
         <select
           className=" block h-10 font-bold w-96 p-2 border border-cyan-400 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:border-transparent"
@@ -131,12 +130,21 @@ const LeaveApplication = () => {
           <option value="casual">Casual Leave</option>
           <option value="annual">Annual Leave</option>
         </select>
-        <div className=" flex  flex-col reasonforleavetext ">
-          <label className="martgintopchange">Reason For the Leave</label>
+        <div className=" flex  flex-col absolute top-[20rem] text-[20px] text-black ml-[33.4rem] ">
+          <label className=" -mt-9">Reason For the Leave</label>
         </div>
-        <input type="text" className=" reasonforleave   "></input>
+        <input
+          style={{
+            width: "30rem",
+            borderWidth: "2px",
+            borderColor: "black",
+            borderSpacing: "2px",
+          }}
+          type="text"
+          className="w-96 border border-spacing-2 border-black"
+        />
       </div>
-      <button className=" bg-blue-900 w-24  rounded-sm my-11 mariginforbuytton">
+      <button className=" bg-blue-900 w-24  rounded-sm my-11 ml-[61rem] text-white w-[7rem] h-[2rem] font-bold">
         Apply Leave
       </button>
     </div>
